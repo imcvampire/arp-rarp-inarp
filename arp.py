e959a4ec6b41cb070be4199fa61bb7f70787db70
clitetailor <ducnhatphung@gmail.com>
2016-05-17 16:33:25 +0700

    send arp request

--- arp.py
+++ arp.py
@@ -1,10 +1,128 @@
 #!/usr/bin/python3
 
 import sys
-import arp_core as arp
+import arp_core
+import socket
+import struct
+import binascii
+import time
+import threading
+
 	
-if __name__ == "__main__":
+
+def main():
 	
+	# Send ARP Request Packets
+	#
+	#
 	for arg in sys.argv:
 		if arg[0:2] == "-a":
-			arp.SendArpRequestPacket(arg[2:])
+			
+			request_ip = arg[2:]
+			
+			# Create Receive Thread
+			#
+			#
+			receive_thread = threading.Thread(target = receiver, args = (request_ip, ))
+			receive_thread.start()
+			
+			# Send ARP Request Packet
+			#
+			#
+			arp_core.SendArpRequestPacket(arg[2:])
+			
+			# Join Thread
+			#
+			#
+			receive_thread.join()
+		#
+		#
+		#
+		# if arg[0:2] == "-a":
+		
+	return
+
+
+def receiver(request_ip):
+	
+	# Receive ARP Reply
+	#
+	# RawSocket
+	#
+	rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+	
+	try:
+		timeout = time.time() + 10
+		
+		while True:
+			# Receive packet
+			#
+			packet = rawSocket.recvfrom(2048)
+			
+			# Unpack Ethernet header
+			#		
+			ethernet_header = packet[0][0:14]
+			ethernet_layer = struct.unpack("!6s6s2s", ethernet_header)
+			
+			# ARP packet filter
+			#
+			ethernet_type = binascii.hexlify(ethernet_layer[2]).decode('ascii')
+			if ethernet_type == "0806":
+				
+				# Unpack ARP header
+				#
+				arp_header = packet[0][14:42]
+				arp_layer = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
+				
+				# Decode packet
+				#
+				#
+				#
+				# Ethernet Layer
+				dest_mac   = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB", ethernet_layer[0])
+				source_mac = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB", ethernet_layer[1])
+				
+				# ARP Layer
+				opcode        = binascii.hexlify(arp_layer[4]).decode('ascii')
+				sender_mac    = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB",arp_layer[5])
+				sender_ip     = socket.inet_ntoa(arp_layer[6])
+				target_mac    = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB",arp_layer[7])
+				target_ip     = socket.inet_ntoa(arp_layer[8])
+				
+				print()
+				if opcode == "0002" and sender_ip == request_ip:
+					
+					print("=================================================")
+					print("IP                     MAC")
+					print()
+					print("{0}            {1}".format(request_ip, sender_mac))
+					print("=================================================")
+					print()
+					break
+		
+			if time.time() > timeout:
+				print()
+				print("=================================================")
+				print("Request Timeout!")
+				print("=================================================")
+				print()
+				break
+		#
+		#
+		#
+		# while true: receive packet	
+			
+	except KeyboardInterrupt:
+		print()
+		print("KeyboardInterrupt!")
+	except Exception as e:
+		print()
+		print(e)
+		print()
+		
+	return
+	
+
+
+if __name__ == "__main__":
+	main()
--- arp_receive.py
+++ arp_receive.py
@@ -6,6 +6,8 @@ import binascii
 import netifaces as ni
 import arp_core
 
+
+
 def main():
 	if __name__ == "__main__":
 		
@@ -18,119 +20,115 @@ def main():
 		
 		try:
 			while True:
-				try:
-					# Receive packet
+				# Receive packet
+				#
+				#
+				#
+				packet = rawSocket.recvfrom(2048)
+				
+				
+				# Unpack Ethernet header
+				#
+				#
+				#			
+				ethernet_header = packet[0][0:14]
+				ethernet_layer = struct.unpack("!6s6s2s", ethernet_header)
+				
+				# ARP packet filter
+				#
+				#
+				#
+				ethernet_type = binascii.hexlify(ethernet_layer[2]).decode('ascii')
+				if ethernet_type == "0806":
+					
+					# Unpack ARP header
 					#
 					#
 					#
-					packet = rawSocket.recvfrom(2048)
+					arp_header = packet[0][14:42]
+					arp_layer = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
+					
+					# Decode packet
+					#
+					#
+					#
+					
+					# Ethernet Layer
+					dest_mac   = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB", ethernet_layer[0])
+					source_mac = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB", ethernet_layer[1])
+					
+					# ARP Layer
+					hardware_type = binascii.hexlify(arp_layer[0]).decode('ascii')
+					protocol_type = binascii.hexlify(arp_layer[1]).decode('ascii')
+					hardware_size = binascii.hexlify(arp_layer[2]).decode('ascii')
+					protocol_size = binascii.hexlify(arp_layer[3]).decode('ascii')
+					opcode        = binascii.hexlify(arp_layer[4]).decode('ascii')
+					sender_mac    = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB",arp_layer[5])
+					sender_ip     = socket.inet_ntoa(arp_layer[6])
+					target_mac    = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB",arp_layer[7])
+					target_ip     = socket.inet_ntoa(arp_layer[8])
 					
 					
-					# Unpack Ethernet header
+					# Packet Information
 					#
 					#
-					#			
-					ethernet_header = packet[0][0:14]
-					ethernet_layer = struct.unpack("!6s6s2s", ethernet_header)
+					#
+					#
+					print()
 					
-					# ARP packet filter
+					count = count + 1
+					print("Packet {0}:".format(count))
+					print()
+					
+					print("================_ETHERNET_FRAME_=================")
+					
+					print("Dest MAC        :", dest_mac)
+					print("Source MAC      :", source_mac)
+					print("Type            :", ethernet_type)
+					
+					print("==================_ARP_HEADER_===================")
+					
+					print("Hardware type   :", hardware_type)
+					print("Protocol type   :", protocol_type)
+					print("Hardware size   :", hardware_size)
+					print("Protocol size   :", protocol_size)
+					print("Opcode          :", opcode)
+					print("Sender MAC      :", sender_mac)
+					print("Sender IP       :", sender_ip)
+					print("Target MAC      :", target_mac)
+					print("Target IP       :", target_ip)
+					
+					print("=================================================")
+					
+					print()
+					
+					# ARP Request Filter
 					#
 					#
 					#
-					ethernet_type = binascii.hexlify(ethernet_layer[2]).decode('ascii')
-					if ethernet_type == "0806":
+					if opcode == "0001":
 						
-						# Unpack ARP header
+						# Internet Interfaces
 						#
 						#
 						#
-						arp_header = packet[0][14:42]
-						arp_layer = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
+						interfaces = arp_core.Interfaces()
 						
-						# Decode packet
-						#
-						#
-						#
+						print("Send Reply:")
 						
-						# Ethernet Layer
-						dest_mac   = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB", ethernet_layer[0])
-						source_mac = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB", ethernet_layer[1])
-						
-						# ARP Layer
-						hardware_type = binascii.hexlify(arp_layer[0]).decode('ascii')
-						protocol_type = binascii.hexlify(arp_layer[1]).decode('ascii')
-						hardware_size = binascii.hexlify(arp_layer[2]).decode('ascii')
-						protocol_size = binascii.hexlify(arp_layer[3]).decode('ascii')
-						opcode        = binascii.hexlify(arp_layer[4]).decode('ascii')
-						sender_mac    = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB",arp_layer[5])
-						sender_ip     = socket.inet_ntoa(arp_layer[6])
-						target_mac    = "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB",arp_layer[7])
-						target_ip     = socket.inet_ntoa(arp_layer[8])
-						
-						
-						# Packet Information
-						#
-						#
-						#
-						#
 						print()
-						
-						count = count + 1
-						print("Packet {0}:".format(count))
-						print()
-						
-						print("================_ETHERNET_FRAME_=================")
-						
-						print("Dest MAC        :", dest_mac)
-						print("Source MAC      :", source_mac)
-						print("Type            :", ethernet_type)
-						
-						print("==================_ARP_HEADER_===================")
-						
-						print("Hardware type   :", hardware_type)
-						print("Protocol type   :", protocol_type)
-						print("Hardware size   :", hardware_size)
-						print("Protocol size   :", protocol_size)
-						print("Opcode          :", opcode)
-						print("Sender MAC      :", sender_mac)
-						print("Sender IP       :", sender_ip)
-						print("Target MAC      :", target_mac)
-						print("Target IP       :", target_ip)
-						
+						print("=================================================")
+						print("Sender IP       :", target_ip)
+						print("Target MAC      :", sender_mac)
+						print("Target IP       :", sender_ip)
 						print("=================================================")
 						
-						print()
-						
-						# ARP Request Filter
-						#
-						#
-						#
-						if opcode == "0001":
-							
-							# Internet Interfaces
-							#
-							#
-							#
-							interfaces = arp_core.Interfaces()
-							
-							print("Send Reply:")
-							
-							print()
-							print("=================================================")
-							print("Sender IP       :", target_ip)
-							print("Target MAC      :", sender_mac)
-							print("Target IP       :", sender_ip)
-							print("=================================================")
-							
-							arp_core.SendArpReplyPacket(target_ip, sender_mac, sender_ip)
-				
-				# try:
-				except Exception as e:
-					print()
-					print(e)
-					
-					print()
-				
+						arp_core.SendArpReplyPacket(target_ip, sender_mac, sender_ip)
+		
+		except Exception as e:
+			print()
+			print(e)
+			print()		
 		except KeyboardInterrupt:
 			print()
 			print("KeyboardInterrupt!")
